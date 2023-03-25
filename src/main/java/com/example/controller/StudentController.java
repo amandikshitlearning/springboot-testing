9d568bd2f4d7e1388439c48daa85a91870385f80
aman-dikshit <60254068+aman-dikshit@users.noreply.github.com>
2023-03-25 17:37:34 +0530

    Student Controller Increments

--- src/main/java/com/example/controller/StudentController.java
+++ src/main/java/com/example/controller/StudentController.java
@@ -5,7 +5,10 @@ import com.example.service.StudentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/student")
@@ -26,4 +29,18 @@ public class StudentController {
         return studentService.saveStudent(student);
     }
 
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Student> getStudent(@PathVariable("id") long id) {
+        log.info("Fetching Student Details with Id {}", id);
+        return studentService.getStudentById(id)
+                             .map(ResponseEntity::ok)
+                             .orElseGet(() -> ResponseEntity.notFound()
+                                                            .build());
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        log.info("Fetching All Students");
+        return studentService.getAllStudents();
+    }
 }
