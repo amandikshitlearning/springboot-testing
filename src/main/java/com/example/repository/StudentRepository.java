69a0a87324e73fcbe32f6fea6dd08e04c1ca6505
aman-dikshit <60254068+aman-dikshit@users.noreply.github.com>
2023-03-25 11:39:40 +0530

    Student Repository Test Case Enhancement

--- src/main/java/com/example/repository/StudentRepository.java
+++ src/main/java/com/example/repository/StudentRepository.java
@@ -2,6 +2,24 @@ package com.example.repository;
 
 import com.example.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    Optional<Student> findByEmail(String email);
+
+    @Query(value = "select s from Student s where s.firstName=?1 and s.lastName=?2")
+    Student findByJpqlIndexedParams(String firstName, String lastName);
+
+    @Query(value = "select s from Student s where s.firstName=:firstName and s.lastName=:lastName")
+    Student findByJpqlNamedParams(String firstName, String lastName);
+
+    @Query(value = "select * from students where first_name=?1 and email=?2", nativeQuery = true)
+    Student findByJpqlNativeQueryIndexedParams(String firstName, String email);
+
+    @Query(value = "select * from students where last_name=:lastName and email=:email", nativeQuery = true)
+    Student findByJpqlNativeQueryNamedParams(String lastName, String email);
+
 }
--- src/test/java/com/example/repository/StudentRepositoryTest.java
+++ src/test/java/com/example/repository/StudentRepositoryTest.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @DataJpaTest
+@DisplayName(value = "Student Repository Test Suite")
 class StudentRepositoryTest {
     @Autowired
     private StudentRepository studentRepository;
@@ -60,5 +61,60 @@ class StudentRepositoryTest {
 //        then - verify output of action/behaviour tested
         Assertions.assertThat(students).hasSize(2);
         Assertions.assertThat(students).isNotEmpty();
+    }
+
+    @Test
+    @DisplayName(value = "Test Find By Email")
+    void givenEmail_whenFindByEmail_thenReturnStudent() {
+//        given - precondition/setup
+        Student s1 = new Student(1, "Virat", "Kohli", "virat@kohli.com");
+        studentRepository.save(s1);
+//        when - action or behaviour to be tested
+        Student dbStudent = studentRepository.findByEmail(s1.getEmail()).get();
+//        then - verify output of action/behaviour tested
+        Assertions.assertThat(dbStudent).isNotNull();
+        Assertions.assertThat(dbStudent.getEmail()).isEqualTo(s1.getEmail());
+    }
+
+    @Test
+    @DisplayName(value = "Test Delete By ID")
+    void givenStudentId_whenDeleteById_thenDeleteStudent() {
+        Student s = new Student(1, "Virat", "Kohli", "virat@kohli.com");
+        studentRepository.save(s);
+        studentRepository.deleteById(s.getId());
+        Optional<Student> optionalStudent = studentRepository.findById(s.getId());
+        Assertions.assertThat(optionalStudent).isEmpty();
+    }
+
+    @Test
+    void givenFirstNameAndLastName_whenFindByJpqlIndexedParams_thenReturnStudent() {
+        Student s = new Student(1, "Virat", "Kohli", "virat@kohli.com");
+        studentRepository.save(s);
+        Student byJpqlIndexedParams = studentRepository.findByJpqlIndexedParams("Virat", "Kohli");
+        Assertions.assertThat(byJpqlIndexedParams).isNotNull();
+    }
+
+    @Test
+    void givenFirstNameAndLastName_whenFindByJpqlNamedParams_thenReturnStudent() {
+        Student s = new Student(1, "Virat", "Kohli", "virat@kohli.com");
+        studentRepository.save(s);
+        Student byJpqlIndexedParams = studentRepository.findByJpqlNamedParams(s.getFirstName(), s.getLastName());
+        Assertions.assertThat(byJpqlIndexedParams).isNotNull();
+    }
+
+    @Test
+    void givenFirstNameAndLastName_whenFindByJpqlNativeQueryIndexedParams_thenReturnStudent() {
+        Student s = new Student(1, "Virat", "Kohli", "virat@kohli.com");
+        studentRepository.save(s);
+        Student byJpqlIndexedParams = studentRepository.findByJpqlNativeQueryIndexedParams(s.getFirstName(), s.getEmail());
+        Assertions.assertThat(byJpqlIndexedParams).isNotNull();
+    }
+
+    @Test
+    void givenFirstNameAndLastName_whenFindByJpqlNativeQueryNamedParams_thenReturnStudent() {
+        Student s = new Student(1, "Virat", "Kohli", "virat@kohli.com");
+        studentRepository.save(s);
+        Student byJpqlIndexedParams = studentRepository.findByJpqlNativeQueryNamedParams(s.getLastName(), s.getEmail());
+        Assertions.assertThat(byJpqlIndexedParams).isNotNull();
     }
 }
