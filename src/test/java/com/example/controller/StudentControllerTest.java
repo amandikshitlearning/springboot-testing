775ad45bbdc322fee73c628b79e73bb315ae4d30
aman-dikshit <60254068+aman-dikshit@users.noreply.github.com>
2023-03-25 17:37:34 +0530

    Student Controller Increments

--- src/test/java/com/example/controller/StudentControllerTest.java
+++ src/test/java/com/example/controller/StudentControllerTest.java
@@ -18,6 +18,10 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 @WebMvcTest(StudentController.class)
 @DisplayName(value = "JUnit Test Case suite for Student Controller")
 class StudentControllerTest {
@@ -49,4 +53,54 @@ class StudentControllerTest {
                 .andDo(MockMvcResultHandlers.print());
     }
 
+    @Test
+    void givenListOfStudents_whenGetAllStudents_thenReturnStudentsList() throws Exception {
+        //        given - precondition/setup
+        Student s1 = Student.builder().firstName("john").lastName("cena").email("john@cena.com").build();
+        Student s2 = Student.builder().firstName("roman").lastName("reigns").email("roman@reigns.com").build();
+        Student s3 = Student.builder().firstName("seth").lastName("rollins").email("seth@rollins.com").build();
+        List<Student> studentList = Arrays.asList(s1, s2, s3);
+        BDDMockito.given(studentService.getAllStudents()).willReturn(studentList);
+        //        when - action/behaviour you want to test
+        ResultActions response = mockMvc.perform(MockMvcRequestBuilders.get("/api/student"));
+        //        then - verify the output
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()", CoreMatchers.is(studentList.size())));
+    }
+
+
+    //    Junit test for Get Student By Id REST API
+    @Test
+    @DisplayName(value = "Junit test for Get Student By Id REST API - Valid Employee ID")
+    void givenStudentID_whenGetStudentByID_thenReturnStudentObject() throws Exception{
+        //        given - precondition/setup
+        long studentId = 1l;
+        Student s1 = Student.builder().firstName("john").lastName("cena").email("john@cena.com").build();
+        BDDMockito.given(studentService.getStudentById(studentId)).willReturn(Optional.of(s1));
+        //        when - action/behaviour you want to test
+        ResultActions response = mockMvc.perform(MockMvcRequestBuilders.get("/api/student/{id}", studentId));
+
+        //        then - verify the output
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName", CoreMatchers.is(s1.getFirstName())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.lastName", CoreMatchers.is(s1.getLastName())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email", CoreMatchers.is(s1.getEmail())));
+    }
+
+    //    Junit test for
+    @Test
+    void givenStudentId_whenGetStudentById_thenReturnNotFound() throws Exception{
+        //        given - precondition/setup
+        long studentId = 1l;
+        BDDMockito.given(studentService.getStudentById(studentId)).willReturn(Optional.empty());
+
+        //        when - action/behaviour you want to test
+        ResultActions response = mockMvc.perform(MockMvcRequestBuilders.get("/api/student/{id}", studentId));
+
+        //        then - verify the output
+        response.andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andDo(MockMvcResultHandlers.print());
+    }
 }
